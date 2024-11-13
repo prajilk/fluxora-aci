e4a8411df40518f571bc82e4fc7d6fbfdac4832e
Prajil K <prajilk20017@gmail.com>
2024-11-14 00:23:31 +0530

    removed unwanted import, added dropdown to useEffect array

--- src/components/contact/object-dropdown.tsx
+++ src/components/contact/object-dropdown.tsx
@@ -24,7 +24,7 @@ const ObjectDropdown = () => {
     } else {
       dropdown.reverse();
     }
-  }, [open]);
+  }, [open, dropdown]);
 
   function handleValue(value: string) {
     setValue(value);
--- src/components/expertise/expertise.tsx
+++ src/components/expertise/expertise.tsx
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import ImageContainer from "./image-container";
 
 const Expertise = () => {
